810e18f868671f16f8c708a8df2d741286e3d22a
martin <guerreiromartin@gmail.com>
2025-09-02 18:49:48 -0300

    cors changed

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -2,16 +2,14 @@ import { CorsOptions } from "cors";
 
 export const corsConfig: CorsOptions = {
     origin: function (origin, callback) {
-        const whiteList = [process.env.FRONTEND_URL]
+        const whiteList = [process.env.FRONTEND_URL];
 
-        if (process.argv[2] === '--api') {
-            whiteList.push(undefined)
-        }
-
-        if (whiteList.includes(origin)) {
+        // Permitir peticiones sin origin (ej. Postman, SSR, etc.)
+        if (!origin || whiteList.includes(origin)) {
             callback(null, true);
         } else {
             callback(new Error("No permitido por CORS"));
         }
-    }
-}
+    },
+    credentials: true,
+};
